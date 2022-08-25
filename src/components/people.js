85cc5669c02331de77c6dde58171d745e5f6db50
unknown <nnamanichidinma203@gmail.com>
2022-08-25 11:57:18 +0100

    initialised react project

---
+++ src/components/people.js
@@ -0,0 +1,19 @@
+class people {
+    constructor (firstName, age, address) {
+        this.firstName = firstName;
+        this.age = age;
+        this.address = address;
+    }
+    people1() {
+        let people1 = new Person();
+        console.log(people1)
+      }
+
+}
+console.log(people)
+
+let people1 = new Person("Dan", 25, "Bauchi");
+let people2 = new Person("Ella", 28, "Lagos");
+let people3 = new Person("Zubby", 30, "Ondo");
+let people4 = new Person("Zinny", 27, "Delta");
+
