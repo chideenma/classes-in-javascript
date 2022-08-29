97ee463558f08d23a680f6b44e4df983b260f95d
unknown <nnamanichidinma203@gmail.com>
2022-08-29 09:42:27 +0100

    created geBirthYear method to show the birth year of the person

--- src/components/people.js
+++ src/components/people.js
@@ -21,12 +21,12 @@ export default function people() {
             this.address = newAddess
         }
         getBirthYear(){
-            new Date().getFullYear() - this.age 
+            this.year = new Date().getFullYear() - Number(this.age)
         }
       
       }
 
-      firstPerson.changePersonName('Mike')
+
 
       console.log(Person);
       // people1() {
@@ -38,9 +38,15 @@ export default function people() {
       const secondPerson = new Person("Ella", 28, "Lagos");
       const thirdPerson = new Person("Zubby", 30, "Ondo");
       const fourthPerson = new Person("Zinny", 27, "Delta");
-      
-      console.log(firstPerson, secondPerson, thirdPerson, fourthPerson)
 
+      firstPerson.changePersonName('Mike')
+
+      firstPerson.getBirthYear();
+      secondPerson.getBirthYear();
+      thirdPerson.getBirthYear();
+      fourthPerson.getBirthYear();
+    
+      console.log(firstPerson, secondPerson, thirdPerson, fourthPerson)
       
       
   return (
